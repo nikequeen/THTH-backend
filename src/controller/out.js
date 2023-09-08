98fe7a77cba2ec7d7cc03945e7facd519ec04473
Audace <adenikeaudace@gmail.com>
2023-09-08 15:58:29 +0100

    model et migration

--- src/controller/out.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const userService = require("../service/userService");
-const router = express.Router();
-
-router.post("/register", userService);
-
-module.exports = router;
