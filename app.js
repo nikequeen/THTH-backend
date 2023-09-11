5d77247be13af2835e73c03a839b4ac270412bfd
Audace <adenikeaudace@gmail.com>
2023-09-11 10:23:29 +0100

    first commit

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ require("dotenv").config();
 
 const express = require("express");
 
-const cors = require('cors');
+const cors = require("cors");
 
-const router = require("./src/routes/router")
+const router = require("./src/routes/router");
+
 
 const app = express();
 
-
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Authorization");
-
-})
-app.use(cors)
+});
+app.use(cors);
 app.use(express.json());
 app.use(express.static("uploads"));
 
-
 router(app);
-
+module.exports = app;
