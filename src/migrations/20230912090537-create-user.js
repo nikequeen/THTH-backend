55428cfcb8473b530b405553e7a187fa8dcb3d59
Audace <adenikeaudace@gmail.com>
2023-09-12 10:21:26 +0100

    first commit

---
+++ src/migrations/20230912090537-create-user.js
@@ -0,0 +1,44 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nom: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      motdepasse: {
+        type: Sequelize.STRING,
+      },
+      status: {
+        type: Sequelize.ENUM("actif", "inactif"),
+        defaultValue: "actif",
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.ENUM("admin", "agent", "client"),
+        defaultValue: "actif",
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
