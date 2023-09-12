62658e808d39c85e72de23a7259740ef466bf701
ambroisehdn <contact@ambroisehdn.me>
2023-09-12 16:33:08 +0200

    add yup validation dto logic

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const userService = require("../services/userService");
-const validateAuth = require("../authDto/validateAuth");
+const yupValidator = require("../middleware/validate");
+const {inscriptionDto} = require("../dto/authDto");
 
-router.post("/inscription", (req, res) => {
-  validateAuth();
-  userService.getUserData();
-  console.log("From Controller AUTH User");
-  return res.status(200).json({ ok: "ok" });
+router.post("/inscription",  yupValidator(inscriptionDto) ,(req, res) => {
+    userService.getUserData();
+    console.log("From Controller AUTH User");
+    return res.status(200).json({ ok: req.body });
 });
 
 module.exports = router;
