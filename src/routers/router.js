64b42855397d49b3154cee04b7ace1462cc3e57d
ambroisehdn <contact@ambroisehdn.me>
2023-09-22 16:29:15 +0200

    start admin action

--- src/routers/router.js
+++ src/routers/router.js
@@ -2,10 +2,11 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const adminController = require("../controllers/admincontolller");
 const {Authmiddleware,typeCompteAuthorisation} = require("../middleware/Authmiddleware");
+const Enumtype = require("../services/utilisateurs/enumtype");
 
 const router = express.Router();
 
 router.use("/authentification", authController);
-router.use("/admin", adminController);
+router.use("/admin",Authmiddleware,typeCompteAuthorisation([Enumtype.Admin]), adminController);
 
 module.exports = router;
