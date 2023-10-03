f8d6316884c380c72ba24750cadc7a5f0bf5fe3e
Audace <adenikeaudace@gmail.com>
2023-10-03 18:53:49 +0100

    ajout de la requete et liste des requete

--- src/controllers/requestController.js
+++ src/controllers/requestController.js
@@ -1,26 +1,55 @@
 const express = require("express");
 const router = express.Router();
-const Requete = require("../services/requete/requeteService");
+const RequeteService = require("../services/requete/requeteService");
 
-router.post("/ajouterunerequete", async function (req, res) {
+router.post("/ajouterunerequete", async (req, res) => {
   try {
-    console.log(req.body);
-    const request = await Requete.createRequete(req.body);
+    const request = await RequeteService.createRequete(req.body);
     if (!request.error) {
       res.status(201).json(request);
     } else {
       res.status(400).json(request);
     }
-    console.log(request);
   } catch (error) {
-    onsole.error(
-      "une erreur c'est produit lors de l'ajout de la requête",
+    console.error(
+      "Une erreur s'est produite lors de l'ajout de la requête",
       error
     );
     res
       .status(500)
-      .json("une erreur c'est produit lors de l'ajout de la requête");
+      .json("Une erreur s'est produite lors de l'ajout de la requête");
   }
 });
+
+router.get("/requeteliste", async (req, res) => {
+  try {
+    const result = await RequeteService.getRequete();
+    res.status(200).json(result);
+  } catch (error) {
+    console.log("Erreur lors de la récupération de toutes les requêtes", error);
+    res
+      .status(404)
+      .json("Erreur lors de la récupération de toutes les requêtes");
+  }
+});
+
+router.put("/updaterequest", async (req, res) => {
+  try {
+    
+    const requeteId = req.params.id;
+    const body = req.body;
+    const result = await RequeteService.updateRequete(requeteId, body);
+
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json("La requête n'a pas été trouvée ou n'a pas été mise à jour.");
+    }
+  } catch (error) {
+    console.log("Erreur lors de la modification de la requête", error);
+    res.status(500).json("Erreur lors de la modification de la requête");
+  }
+});
+
 
 module.exports = router;
--- src/services/requete/requeteService.js
+++ src/services/requete/requeteService.js
@@ -1,25 +1,42 @@
-const queryClass =require("../../utils/queryClass");
-const { Requete } = require("../../models")
+const queryClass = require("../../utils/queryClass");
+const { Requete } = require("../../models");
 
 class RequeteService extends queryClass {
+  async createRequete(donneCreation) {
+    const nom = donneCreation.nom;
+    try {
+      const existingRequete = await this.findOne({ nom: nom });
+      console.log(existingRequete);
 
-    async createRequete(donneCreation) {
-        const nom = donneCreation.nom;
-        try {
-          const existingRequete = await this.findOne({ nom: nom });
-          console.log(existingRequete);
-    
-          if (existingRequete) {
-            return { error: true, message: "Cette requête est déja présente" };
-          }
-          const newRequest = await this.create(donneCreation);
-          console.log("Created new record:", newRequest);
-          return { error: false, requete: newRequest };
-        } catch (error) {
-          console.error("Error creating the request:", error);
-          return { error: true, error: error };
-        }
+      if (existingRequete) {
+        return { error: true, message: "Cette requête est déja présente" };
       }
+      const newRequest = await this.create(donneCreation);
+      console.log("Created new record:", newRequest);
+      return { error: false, requete: newRequest };
+    } catch (error) {
+      console.error("Error creating the request:", error);
+      return { error: true, error: error };
+    }
+  }
+
+  async getRequete() {
+    try {
+      const Requete = await this.findAll();
+      return Requete;
+    } catch (error) {
+      throw new Error("Failed to get the request" + error.message);
+    }
+  }
+  async updateRequete(id, nouvellesDonnees) {
+    try {
+      const requete = await this.update({ _id: id }, nouvellesDonnees);
+      return requete;
+    } catch (error) {
+      throw new Error("Échec de la mise à jour de la requête: " + error.message);
+    }
+  }
+
 }
 
 module.exports = new RequeteService(Requete);
